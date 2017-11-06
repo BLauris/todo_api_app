4aa574e7b7a871fed40fc7218ee7caad1fed8487
BLauris <bligzna.lauris@gmail.com>
2017-11-06 16:20:03 +0200

    Added has_many tags to TaskSerializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,6 +1,8 @@
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :created_at, :updated_at
   
+  has_many :tags
+  
   def created_at
     object.created_at.strftime("%d/%m/%Y")
   end
